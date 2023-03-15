9f54287524fcd00a4b9f5b93f5829beeb7d975e7
PedrovRamos <pvramos.cunha@gmail.com>
2023-03-15 00:29:30 -0300

    Enviando projeto com req 1, 2, 3 e 5 concluidos

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
-const { readTalkerData } = require('./utils/fsUtils');
+const { readTalkerData, writeNewTalkerData } = require('./utils/fsUtils');
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_CREATED_STATUS = 201;
+const HTTP_FAIL_STATUS = 404;
 const PORT = process.env.PORT || '3001';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -16,6 +18,35 @@ app.get('/', (_request, response) => {
 app.get('/talker', async (_request, response) => {
   const talker = await readTalkerData();
   response.status(HTTP_OK_STATUS).send(talker);
+});
+
+app.get('/talker/:id', async (_request, response) => {
+  try {
+    const talker = await readTalkerData();
+    const { id } = _request.params;
+    const chosenTalker = talker.find((tal) => tal.id === Number(id));
+    if (chosenTalker === undefined) throw new Error('pessoa palestrante não encontrada');
+    response.status(HTTP_OK_STATUS).send(chosenTalker);
+  } catch (err) {
+    response.status(HTTP_FAIL_STATUS).send({ message: err.message });
+  }
+});
+
+app.post('/talker', async (_request, response) => {
+  const newTalker = _request.body;
+  const newTalkerWrite = await writeNewTalkerData(newTalker);
+  response.status(HTTP_CREATED_STATUS).send(newTalkerWrite);
+});
+
+app.post('/login', async (_request, response) => {
+  // const { email, password } = _request.body;
+
+  // tokenGen
+  
+  const token = (Math.random().toString(16).substring(2)
+  + Math.random().toString(16).substring(2)).substring(0, 16);
+
+  response.status(HTTP_OK_STATUS).send({ token });
 });
 
 app.listen(PORT, () => {
--- src/utils/fsUtils.js
+++ src/utils/fsUtils.js
@@ -1,9 +1,11 @@
 const fs = require('fs').promises;
 const path = require('path');
 
+const MISSION_DATA_PATH = '../talker.json';
+
 async function readTalkerData() {
     try {
-        const data = await fs.readFile(path.resolve(__dirname, '../talker.json'));
+        const data = await fs.readFile(path.resolve(__dirname, MISSION_DATA_PATH));
         const talker = JSON.parse(data);
         return talker;
     } catch (err) {
@@ -11,6 +13,18 @@ async function readTalkerData() {
     }
 }
 
+async function writeNewTalkerData(newTalker) {
+    try {
+        const oldTalker = await readTalkerData();
+        const allTalker = JSON.stringify([...oldTalker, newTalker]);
+        await fs.writeFile(path.resolve(__dirname, MISSION_DATA_PATH), allTalker);
+        return newTalker;
+    } catch (err) {
+        console.log(`erro na escrita do arquivo: ${err}`);
+    }
+}
+
 module.exports = {
     readTalkerData,
+    writeNewTalkerData,
 };
